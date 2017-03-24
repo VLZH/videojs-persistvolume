60e81107616b373bc970f8e34c804134b1c927b6
Vladimir Zhdanov <vladimirzhdanov1992@gmail.com>
2017-03-24 16:51:11 +0600

    Set volume from localStorage on change of urls

--- videojs.persistvolume.js
+++ videojs.persistvolume.js
@@ -1,5 +1,5 @@
 "use strict";
-(function(factory){
+(function (factory) {
   /*!
    * Custom Universal Module Definition (UMD)
    *
@@ -8,115 +8,121 @@
    * compiler compatible, so string keys are used.
    */
   if (typeof define === 'function' && define['amd']) {
-    define(['./video'], function(vjs){ factory(window, document, vjs) });
-  // checking that module is an object too because of umdjs/umd#35
+    define(['./video'], function (vjs) {
+      factory(window, document, vjs)
+    });
+    // checking that module is an object too because of umdjs/umd#35
   } else if (typeof exports === 'object' && typeof module === 'object') {
     factory(window, document, require('video.js'));
   } else {
     factory(window, document, videojs);
   }
 
-})(function(window, document, vjs) {
+})(function (window, document, vjs) {
   //cookie functions from https://developer.mozilla.org/en-US/docs/DOM/document.cookie
   var
-  getCookieItem = function(sKey) {
-    if (!sKey || !hasCookieItem(sKey)) { return null; }
-    var reg_ex = new RegExp(
-      "(?:^|.*;\\s*)" +
-      window.escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
-      "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"
-    );
-    return window.unescape(document.cookie.replace(reg_ex,"$1"));
-  },
-
-  setCookieItem = function(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return; }
-    var sExpires = "";
-    if (vEnd) {
-      switch (vEnd.constructor) {
-        case Number:
-          sExpires = vEnd === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + vEnd;
-          break;
-        case String:
-          sExpires = "; expires=" + vEnd;
-          break;
-        case Date:
-          sExpires = "; expires=" + vEnd.toGMTString();
-          break;
+    getCookieItem = function (sKey) {
+      if (!sKey || !hasCookieItem(sKey)) {
+        return null;
       }
-    }
-    document.cookie =
-      window.escape(sKey) + "=" +
-      window.escape(sValue) +
-      sExpires +
-      (sDomain ? "; domain=" + sDomain : "") +
-      (sPath ? "; path=" + sPath : "") +
-      (bSecure ? "; secure" : "");
-  },
+      var reg_ex = new RegExp(
+        "(?:^|.*;\\s*)" +
+        window.escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
+        "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"
+      );
+      return window.unescape(document.cookie.replace(reg_ex, "$1"));
+    },
 
-  hasCookieItem = function(sKey) {
-    return (new RegExp(
-      "(?:^|;\\s*)" +
-      window.escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
-      "\\s*\\=")
-    ).test(document.cookie);
-  },
-
-  hasLocalStorage = function() {
-    try {
-      window.localStorage.setItem('persistVolume', 'persistVolume');
-      window.localStorage.removeItem('persistVolume');
-      return true;
-    } catch(e) {
-      return false;
-    }
-  },
-  getStorageItem = function(key) {
-    return hasLocalStorage() ? window.localStorage.getItem(key) : getCookieItem(key);
-  },
-  setStorageItem = function(key, value) {
-    return hasLocalStorage() ? window.localStorage.setItem(key, value) : setCookieItem(key, value, Infinity, '/');
-  },
-
-  extend = function(obj) {
-    var arg, i, k;
-    for (i = 1; i < arguments.length; i++) {
-      arg = arguments[i];
-      for (k in arg) {
-        if (arg.hasOwnProperty(k)) {
-          obj[k] = arg[k];
+    setCookieItem = function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+        return;
+      }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Tue, 19 Jan 2038 03:14:07 GMT" : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toGMTString();
+            break;
         }
       }
-    }
-    return obj;
-  },
+      document.cookie =
+        window.escape(sKey) + "=" +
+        window.escape(sValue) +
+        sExpires +
+        (sDomain ? "; domain=" + sDomain : "") +
+        (sPath ? "; path=" + sPath : "") +
+        (bSecure ? "; secure" : "");
+    },
 
-  defaults = {
-    namespace: ""
-  },
+    hasCookieItem = function (sKey) {
+      return (new RegExp(
+        "(?:^|;\\s*)" +
+        window.escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") +
+        "\\s*\\=")).test(document.cookie);
+    },
 
-  volumePersister = function(options) {
-    var player = this;
-    var settings = extend({}, defaults, options || {});
+    hasLocalStorage = function () {
+      try {
+        window.localStorage.setItem('persistVolume', 'persistVolume');
+        window.localStorage.removeItem('persistVolume');
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+    getStorageItem = function (key) {
+      return hasLocalStorage() ? window.localStorage.getItem(key) : getCookieItem(key);
+    },
+    setStorageItem = function (key, value) {
+      return hasLocalStorage() ? window.localStorage.setItem(key, value) : setCookieItem(key, value, Infinity, '/');
+    },
 
-    var key = settings.namespace + '-' + 'volume';
-    var muteKey = settings.namespace + '-' + 'mute';
+    extend = function (obj) {
+      var arg, i, k;
+      for (i = 1; i < arguments.length; i++) {
+        arg = arguments[i];
+        for (k in arg) {
+          if (arg.hasOwnProperty(k)) {
+            obj[k] = arg[k];
+          }
+        }
+      }
+      return obj;
+    },
 
-    player.on("volumechange", function() {
-      setStorageItem(key, player.volume());
-      setStorageItem(muteKey, player.muted());
-    });
+    defaults = {
+      namespace: ""
+    },
 
-    var persistedVolume = getStorageItem(key);
-    if(persistedVolume !== null){
-      player.volume(persistedVolume);
-    }
+    volumePersister = function (options) {
+      var player = this;
+      var settings = extend({}, defaults, options || {});
 
-    var persistedMute = getStorageItem(muteKey);
-    if(persistedMute !== null){
-      player.muted('true' === persistedMute);
-    }
-  };
+      var key = settings.namespace + '-' + 'volume';
+      var muteKey = settings.namespace + '-' + 'mute';
+
+      player.on("volumechange", function () {
+        setStorageItem(key, player.volume());
+        setStorageItem(muteKey, player.muted());
+      });
+      player.on("ready", function () {
+        var persistedVolume = getStorageItem(key);
+        if (persistedVolume !== null) {
+          player.volume(persistedVolume);
+        }
+
+        var persistedMute = getStorageItem(muteKey);
+        if (persistedMute !== null) {
+          player.muted('true' === persistedMute);
+        }
+      })
+    };
 
   vjs.plugin("persistvolume", volumePersister);
 
